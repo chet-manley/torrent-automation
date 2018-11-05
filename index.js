b397174aeb68d14ce5427cabf7e58c371bc0de59
Chet Manley <joshua.gardner@hotmail.com>
2018-11-05 09:38:05 -0600

    Recreate app layout

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 #!/bin/env node
 'use strict'
 
-/*
-const processType = process.env.PROCESS_TYPE || 'main'
-let child
-
-try {
-  child = require(`./workers/${processType}`)
-} catch (ex) {
-  if (ex.code === 'MODULE_NOT_FOUND') {
-    throw new Error(`No worker for: ${processType}`)
-  }
-  throw ex
-}
-*/
-
-//module.exports = child
+module.exports = require('./src')
---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/*
+const processType = process.env.PROCESS_TYPE || 'main'
+let child
+
+try {
+  child = require(`./workers/${processType}`)
+} catch (ex) {
+  if (ex.code === 'MODULE_NOT_FOUND') {
+    throw new Error(`No worker for: ${processType}`)
+  }
+  throw ex
+}
+*/
+
+//module.exports = child
