74f74f7adc55d05148a8ab4b41958a169deb5674
Chet Manley <joshua.gardner@hotmail.com>
2018-11-12 10:55:45 -0600

    Module progress

--- src/lib/Logger/Logger.js
+++ src/lib/Logger/Logger.js
@@ -2,6 +2,25 @@
 
 const config = require('../../config')
 const winston = require('winston')
-const Logger = {}
 
-module.exports = Logger
+const transports = [
+  new winston.transports.Console({
+    colorize: false,
+    json: false,
+    handleExceptions: !config.throw,
+  })
+]
+
+const logger = winston.createLogger({
+  level: config.priority,
+  levels: config.levels,
+  transports,
+  exitOnError: true,
+  silent: !config.enabled
+})
+
+function newLogger (options = {}) {
+  return
+}
+
+module.exports = Logger || createLogger()
